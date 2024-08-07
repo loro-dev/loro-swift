c44cf4bd2544c41fc7ae996f91eafc2b088f48fe
Leon Zhao <leeeon233@gmail.com>
2024-08-07 14:43:04 +0800

    chore: add ci

--- scripts/refine_trait.sh
+++ scripts/refine_trait.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-file_path="Sources/Loro/loroFFI.swift"
+set -euxo pipefail
+THIS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SWIFT_FOLDER="$THIS_SCRIPT_DIR/../gen-swift"
+file_path="$SWIFT_FOLDER/loro.swift"
 
 search_string="public protocol LoroValueLike : AnyObject {"
 replace_string="public protocol LoroValueLike : Any {"
